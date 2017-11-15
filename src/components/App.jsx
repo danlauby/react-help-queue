bea647a06427af7c6391e7024f31f6e7e9706a45
Dan Lauby <danlauby@Dans-MacBook-Pro.local>
2017-11-15 15:46:29 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketLIst";
+
+function App(props) {
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(props) {
+  return (
+    <Ticket
+      location="3A"
+      names="Trent and Dan"
+      issue="Playing with Legos"
+      />
+  );
+}
+
+export default TicketList;
