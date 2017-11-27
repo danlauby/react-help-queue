5568288ad9479f30f337d5ebdb8f0cf492912f70
Dan Lauby <danlauby@Dans-MacBook-Pro.local>
2017-11-27 14:25:45 -0800

    Add hot loading and replace index.html with template.ejs for embedding js.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketLIst";
+import TicketList from "./TicketList";
 
 function App(props) {
   return (
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ticket(props) {
-  return(
-    <div>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
-      <hr/>
-    </div>
-  );
-}
+  var ticketStyles = {
+    backgroundColor: "rgb(161, 195, 139)",
+    fontFamily: "Roboto",
+    padding: "10px",
+    border: "1px solid rgb(55, 85, 29)",
+    borderRadius: "10px"
+  }
+    return(
+      <div style={ticketStyles}>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
+      </div>
+    );
+  }
+
+
+Ticket.propTypes = {
+  location: PropTypes.string.isRequired,
+  names: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
 
 export default Ticket;
